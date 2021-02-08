e7fda45c3ee0834670e5c305a766d3b627c72fe4
cuanoconnor <cuanoconnor@DESKTOP-9IA7HB6.localdomain>
2021-02-08 23:15:57 +0000

    more bit operations work

--- bitoperations.c
+++ bitoperations.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
 
-void bin_print(int i)
+void bin_print(unsigned int i)
 {
     // Number of bits in an integer.
-    int j = (sizeof(int) * 8);
+    int j = (sizeof(unsigned int) * 8);
 
     // temp variable
     int k;
 
+    // Loop over the num of bits in i, left to right.
     for(j-- ; j >= 0; j--)
     {
-        k = ((1 << j) & i) ? 1: 0;
+        // pick out the J^th bit of i
+        k = ((1 << j) & i) ? 1 : 0;
+        // print k
         printf("%d", k);
     }
 }
 
+// standard main signature
 int main(int argc, char *argv[]) 
 {
-    int i = 241;
+    // set i to a literal value.
+    unsigned int i = 0x0f0f0f0f; // 241;
 
-    printf("Original:  ");
+    // What we're printing
+    printf("Original:\t");
+    // print i in binary
     bin_print(i);
-    printf("\n");
+    // end line
+    printf("\t%x\t%u\n\n",i, i);
 
-    for (int j = 0; j < 40; j++)
+    // 32
+    int j = sizeof(unsigned int) * 8;
+
+    for (j--; j >= 0; j--)
     {
-        printf("%3d << %2d: ", i, j);
-        bin_print(i << j);
-        printf("\n");
+        // 1 shifted left j times.
+        bin_print(1 << j); printf("\n");
+        // i.
+        bin_print(i); printf("\n");
+        // (1 shifted left j times) bitwise logical and i.
+        printf("-------------------------------- &\n");
+        bin_print(((1 << j) & i)); printf("\n\n");
     }
     
+    // Everything is ok
     return 0;
 }
